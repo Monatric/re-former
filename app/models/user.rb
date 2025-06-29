fcbf15ebd816e018639a8bb9519ec9501c597272
Monatric <nawpnawp8@gmail.com>
2025-06-30 00:56:22 +0800

    fix: user column validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :name, presence: true, uniqueness: true, length: { in: 3..12 }
+  validates :username, presence: true, uniqueness: true, length: { in: 3..12 }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
 end
